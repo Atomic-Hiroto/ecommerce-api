1b9f625663839faaac8d4b8a597d71a31e9822be
Pritam <borolepritam@gmail.com>
2023-07-26 18:48:14 +0530

    Changed rate limit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const rateLimit = require('express-rate-limit');
 // Limiter Middleware for Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests  
+  max: 500 // limit each IP to 100 requests  
 });
 
 // Routers
